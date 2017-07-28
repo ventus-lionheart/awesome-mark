fe1140daee13d0d838612a7d76cd39cce2a3ba0c
Ananda Chowdhury <Ananda@anandas-mbp.attlocal.net>
2017-07-28 16:57:46 -0400

    Create bookman component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookmarkComponent } from './components/bookmark/bookmark.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookmarkComponent
   ],
   imports: [
     BrowserModule
